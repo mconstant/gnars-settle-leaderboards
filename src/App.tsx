bc8f127e099420bd329c1e059d4171e482ae4873
Anonymous <satoshin@gmx.net>
2023-06-20 18:50:11 -0400

    start to add the leaderboard to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useQuery} from "@apollo/client";
+import { settlements } from '../wrappers/subgraph';
+import {useCoingeckoPrice} from "@usedapp/coingecko";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(settlements(tops || 10));
+    const etherPrice = Number(useCoingeckoPrice('ethereum', 'usd'));
+
+    if (loading) {
+        return (
+            <div>
+                <b>loading...</b>
+            </div>
+        );
+    } else if (error) {
+        return (
+            <div>
+                <b>error...</b>
+            </div>
+        );
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+
+                <p>
+                    gnars settlement leaderboards
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
