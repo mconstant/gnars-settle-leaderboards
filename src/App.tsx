8a9272f86bd609f40019d96b95a41ac0286a4339
Anonymous <satoshin@gmx.net>
2023-06-22 09:15:18 -0400

    take out logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {useQuery} from "@apollo/client";
 import {settlements} from './wrappers/subgraph';
@@ -28,8 +27,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-
                 <p>
                     gnars settlement leaderboards
                 </p>
